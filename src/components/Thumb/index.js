947f9053330203ee320e3797503b88cc8700cd6d
Justice Essuman <justice.essuman@turntbl>
2021-09-05 01:47:55 +0000

    added routes and hook for movie id

--- src/components/Thumb/index.js
+++ src/components/Thumb/index.js
@@ -1,10 +1,19 @@
 import React  from 'react';
+import { Link } from 'react-router-dom';
 
 import { Image }  from './Thumb.styles';
 
 const Thumb = ({image, movieId, clicker})=> (
     <div>
-        <Image src={image} alt='movie-thumb' />
+        { clicker ? 
+            (
+                <Link to={`/${movieId}`}>
+                    <Image src={image} alt='movie-thumb' />
+                </Link>
+            ): (
+                <Image src={image} alt='movie-thumb' />
+            ) 
+        }
     </div>
 );
 
