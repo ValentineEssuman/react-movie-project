caa7b2d8f1cb2e429c327b9ab82e71a24bcc5cc4
Justice Essuman <justice.essuman@turntbl>
2021-09-08 17:26:33 +0000

    styled navigation

--- src/components/BreadCrumb/BreadCrumb.styles.js
+++ src/components/BreadCrumb/BreadCrumb.styles.js
@@ -2,10 +2,27 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
     background-color: brown;
-    margin: 10px;
     display: flex;
     justify-content: center;
-    width: 100px;
+    align-items: center;
+    width: 100%;
+    height: 70px;
+    color: var(--white);
 `;
 
-export const Content = styled.div``;
+export const Content = styled.div`
+    background-color: orange;
+    display: flex;
+    width: 100%;
+    padding 0 20px;
+
+    span {
+        font-size: var(--fontMed);
+        color: var(--white);
+        padding-right: 10px;
+
+        @media screen and (max-width: 768px){
+            font-size: var(--fontSmall)
+        }
+    }
+`;
